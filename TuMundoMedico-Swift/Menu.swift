7b2e5a5fb96d1a43aa8cf699d61ad8b37d971749
Julio E. Ortiz <2502022019@mail.utec.edu.sv>
2023-11-26 10:19:18 -0600

    añadiendo todos los cambios relaciondos para este menu

---
+++ TuMundoMedico-Swift/Menu.swift
@@ -0,0 +1,18 @@
+//
+//  Menu.swift
+//  TuMundoMedico-Swift
+//
+//  Created by Enmanuel on 23/11/23.
+//
+
+import SwiftUI
+
+struct Menu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Menu()
+}
---
+++ TuMundoMedico-Swift/MenuView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuView.swift
+//  TuMundoMedico-Swift
+//
+//  Created by Enmanuel on 23/11/23.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuView()
+}
